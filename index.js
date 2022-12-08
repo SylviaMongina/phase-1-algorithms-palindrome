7039c13c0c25a10d3d7f0a8e9fa2a5aa312a7bc3
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2022-12-08 13:21:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // this lets one compare the reversed string to the input
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+   
+  reverse the input string
+  if the reversed string is the same as the input
+    return true
+  else
 */
 
 /*
   Add written explanation of your solution here
+  Question:Write a function isPalindrome that will receive one argument, a string. 
+  Your function should return true if the string is a palindrome
+   (that is, if it reads the same forwards and backwards, like "mom" or "racecar"), 
+   and return false if it is not a palindrome
 */
 
 // You can run `node index.js` to view these console logs
@@ -18,6 +36,33 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
+
+  console.log("");
+
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("dad"));
+
+  console.log("");
+
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("sylvia"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("bosire"));
+
+  console.log("");
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
